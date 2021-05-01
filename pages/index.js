55316d128db0080d179c5c394508d3cbfcf8fa9d
ryo0705s <inspire.sasaki15@gmail.com>
2021-05-01 14:47:37 +0900

    編集機能リファクタリング開始

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ function Home () {
   // const router = useRouter();
   const [valueTodo, setValueTodo] = useState('')
   const [valueLimit, setValueLimit] = useState('')
-  // const [valueState, setValueState] = useState('')
   const [states, setStates] = useState([])
-  // const todos = [valueTodo, valueLimit, valueState]
   const addTodo= text => {
     const newTodo = [...states, {text, complete: false, edit: false}]
     setStates(newTodo)
@@ -23,7 +21,6 @@ function Home () {
   const handleSubmit = e => {
     e.preventDefault()
     addTodo([valueTodo, valueLimit])
-    // console.log(handleSubmit)
   }
   const deleteStates = index => {
     const newTodo = [...states]
@@ -34,26 +31,30 @@ function Home () {
     const newTodo = [...states]
     newTodo[index].complete = !newTodo[index].complete
     setStates(newTodo)
-    // console.log({changeStates})
   }
   const cancelStates = () => {
     setValueTodo('')
     setValueLimit('')
   }
-  const editStates = index => {
-    // router.push({
-    //   pathname:"/edit",
-    //   query: {state :state} 
-    // });
-    const newTodo = [...states]
-    newTodo[index].edit = !newTodo[index].edit
-    setStates(newTodo)
-    // console.log({editStates})
+  // const editStates = index => {
+  //   // router.push({
+  //   //   pathname:"/edit",
+  //   //   query: {state :state} 
+  //   // });
+  //   const newTodo = [...states]
+  //   newTodo[index].edit = !newTodo[index].edit
+  //   setStates(newTodo)
+  // } 
+  const editStates = () => {
+   const newTodo = [...states]
+   setStates(newTodo)
   } 
-  const updateStates = e => {
-    // // state.value
-    // <div>{e.target.value}</div>
-    console.log(e.target.value)
+  const updateValues = e => {
+    const newTodo = [...states]
+    newTodo[0] = {text: e.target.value, complete: false, edit: false}
+    setStates(newTodo)
+  }
+  const updateLimits = e => {
     const newTodo = [...states]
     newTodo[0] = {text: e.target.value, complete: false, edit: false}
     setStates(newTodo)
@@ -63,11 +64,8 @@ function Home () {
     <Layout>
     <Head>
       <title>todoアプリ</title>
-      {/* <div>{test}</div>
-      <div>{teston}</div> */}
     </Head>
     <h1>ToDoリスト</h1>
-    {/* <div>{state.text[1]}</div> */}
     <Image 
       src='/images/profile.jpg' 
       height={180}
@@ -78,8 +76,6 @@ function Home () {
     <input value={valueTodo} onChange={e => setValueTodo(e.target.value)}/>
     <div>期限</div>
     <input value={valueLimit} onChange={e => setValueLimit(e.target.value)}/>
-    {/* <div>状態</div>
-    <input value={valueState} onChange={e => setValueState(e.target.value)}/> */}
     <button value={addTodo} onClick={handleSubmit}>追加</button>
     <button onClick={cancelStates}>キャンセル</button>
     <table>
@@ -95,30 +91,23 @@ function Home () {
         {states && states.map((state, index) => (
           <tr key={index}>
             <td>{index + 1}</td>
-            <td>{state.text[0]}</td>
             <td>
-              {/* {state.onClick={editStates} ? <input value={state.text[0]} /> : {state.text[0]}} */}
-              {/* {1 == 1 ? <input value={state.text[0]} /> : state.text[0]} */}
-              {/* {state.edit ? <input value={state.text[1]} onChange={ e => setStates(e.target.value)}/> : state.text[1]} */}
-              {state.edit ? <input type='text' value={state.text[1]} onChange={updateStates}/> : state.text[1]}
-              {/* if (state={editStates}) = {
-                return <input value={state.text[0]} />
-              } else {
-                retrun state.text[0]
-              }
-            */}
+              {/* {state.edit ? <input type='text' value={state.text[0]} onChange={updateValues}/> : state.text[0]} */}
+              {state.text[0]}
             </td>
-            {/* <td>{state.text[1]}</td> */}
             <td>
-              {/* <button onClick={changeStates}> */}
-              {/* <button onClick={ index => changeStates}> */}
+              {/* {state.edit ? <input type='text' value={state.text[1]} onChange={updateLimits}/> : state.text[1]} */}
+              {state.text[1]}
+            </td>
+            <td>
               <button onClick={() => changeStates(index)}>
                 {state.complete ? '完了' : '未完了'}
               </button>
             </td>
             <td>  
               {/* <Link href="/edit"> */}
-                <button onClick={() => editStates(index)}>編集</button>  
+                {/* <button onClick={() => editStates(index)}>編集</button>   */}
+                <button onClick={editStates}>編集</button>  
                 {/* <button>編集</button>   */}
               {/* </Link> */}
             </td>
@@ -127,6 +116,24 @@ function Home () {
             </td>
           </tr>
         ))}
+      </tbody>
+    </table>
+    <table>
+      <thead>
+          <h2>編集中</h2>
+          <tr>
+          <th>ID</th>
+          <th>やること</th>
+          <th>期限</th>
+        </tr>
+      </thead>
+      <tbody>
+        {states && states.map((state, index) =>{
+        <tr key={index}>
+          <td><input type='text' value={state.text[0]}/></td>  
+          <td><input type='text' value={state.text[1]}/></td>
+        </tr>
+        })}
       </tbody>
     </table>
     </Layout>
